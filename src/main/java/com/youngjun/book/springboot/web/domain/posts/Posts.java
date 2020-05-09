cb9aab81e92e663e0f1ad57d9247cc91c6dc8433
SohnYoungJun <sohnyj37@gmail.com>
2020-05-09 23:01:14 +0900

    [Add] Posts 클래스 작성

--- src/main/java/com/youngjun/book/springboot/web/domain/posts/Posts.java
+++ src/main/java/com/youngjun/book/springboot/web/domain/posts/Posts.java
@@ -1,12 +1,33 @@
 package com.youngjun.book.springboot.web.domain.posts;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
-@EntityScan
-public class Posts {
+@Entity // Java Persistence Api annotation 를 사용한다.
+public class Posts { //실제 DB 테이블과 매칭될 클래스
+
+    @Id //
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }//Posts
 
 }//Posts
