06ae4721ef302e521f2341c5e38101c229b0158f
SohnYoungJun <sohnyj37@gmail.com>
2020-05-15 23:19:49 +0900

    [Add] mustache template 엔진 추가
    * mustache 템플릿 엔진을 사용하는 이유
    1. JSP/Velocity 가 스프링부트에서 권장되지 않는 템플릿 엔진이다.
    2. 문법이 다른 템플릿 엔진보다 심플하다.
    3. 로직 코드를 사용할 수 없지만 이로인해 View 의 역할과 서버의 역할을 명확하게
    구분할수 있다.

---
+++ src/main/java/com/youngjun/book/springboot/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.youngjun.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+
+        return "/index"; //index 페이지로 보냄
+    }//index()
+}//IndexController
