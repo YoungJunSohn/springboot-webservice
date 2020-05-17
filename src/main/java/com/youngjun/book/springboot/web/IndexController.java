8908bc43e08faef1284aa51affae1516639b55bf
SohnYoungJun <sohnyj37@gmail.com>
2020-05-17 23:20:06 +0900

    [Add] 게시글 수정 화면
    * posts-update 페이지
    * 이미 만들어진 api로 요청하여 게시글 수정

--- src/main/java/com/youngjun/book/springboot/web/IndexController.java
+++ src/main/java/com/youngjun/book/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.youngjun.book.springboot.web;
 
+import com.youngjun.book.springboot.web.dto.PostsResponseDto;
 import com.youngjun.book.springboot.web.service.posts.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -24,5 +26,10 @@ public class IndexController {
         return "posts-save";
     }//postsSave
 
-
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postService.findById(id); //id로
+        model.addAttribute("post", dto);
+        return "posts-update";
+    }//postsUpdate
 }//IndexController
