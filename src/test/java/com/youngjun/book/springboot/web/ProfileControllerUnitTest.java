d8b0ed1bea2b9651c52e433cd19b3ad686ed3fe1
SohnYoungJun <sohnyj37@gmail.com>
2020-05-30 00:40:55 +0900

    [Add] Nginx port 관리 API 작성
    * ProfileController 작성

---
+++ src/test/java/com/youngjun/book/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,64 @@
+package com.youngjun.book.springboot.web;
+
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile_inquiry(){
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }//real_profile_inquiry()
+
+
+
+
+    @Test
+    public void if_realfile_doesnot_exist(){
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+
+    }//if_realfile_doesnot_exist
+
+
+    @Test
+    public void if_active_profile_doesnot_exist(){
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }//if_active_profile_doesnot_exist
+}//ProfileControllerTest
