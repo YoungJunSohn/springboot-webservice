121721f687f28e919a5d2225fc80591f18400b7c
SohnYoungJun <sohnyj37@gmail.com>
2020-06-03 23:19:16 +0900

    [Test] 스크립트 수정
    * profile.sh, health.sh 수정 및 배포 테스트

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#profile.sh : 스크립트를 실행시켜줄 프로그램의 경로 지정
 
 #실행되지 않은 profile 찾기 : real1이 사용중인 경우 , real2가 실행되지 않고 있음
 function find_idle_profile()
@@ -35,4 +36,4 @@ function find_idle_port()
     fi
 }
 #bash 는 값은 리턴하는 기능이 없다. 따라서 제일 마지막 줄에 echo로 결과를 출력한 후,
-#클라이언트에서 그 값은 잡아 사용한다. return 키워드와 마찬가지로 echo를 function중간에 사용해서는 안된다.
+#클라이언트에서 그 값을 잡아 사용한다. return 키워드와 마찬가지로 echo를 function중간에 사용해서는 안된다.
