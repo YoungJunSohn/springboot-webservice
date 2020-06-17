bf790b65c58f5e7d417f04493690e55f8520ab69
SohnYoungJun <sohnyj37@gmail.com>
2020-06-17 22:56:50 +0900

    [Add] template 수정 완료
    * logo 제작 및 추가
    * template 위치 및 크기변경
    * 게시판 글쓰기 권한 - 로그인 시 GUEST 권한 획득 방식에서 USER 권한 획득
    방식으로 변경
    * 게시판 card 형식 dateformat을 dao에서 변경후 전달하는 방식으로 변경

--- src/main/java/com/youngjun/book/springboot/web/domain/posts/Posts.java
+++ src/main/java/com/youngjun/book/springboot/web/domain/posts/Posts.java
@@ -24,11 +24,14 @@ public class Posts extends BaseTimeEntity { //실제 DB 테이블과 매칭될 �
 
     private String author;
 
+    private int bgNum;
+
     @Builder
     public Posts(String title, String content, String author){
         this.title = title;
         this.content = content;
         this.author = author;
+        this.bgNum = (int)Math.floor(Math.random()*10+1);
     }//Posts
 
 
